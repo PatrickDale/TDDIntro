5c4c2193bc27178e8755094ed647bca90540d9d1
PatrickDale <patdale216@gmail.com>
2015-07-21 15:05:17 -0700

    Test shouldThrowIllegalArgumentExceptionWhenNumberIsNegative passing

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -1,7 +1,11 @@
 package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
-    public Integer compute(int i) {
-        return -1;
+    public Integer compute(int i) throws IllegalArgumentException {
+        if (i < 0) {
+            throw new IllegalArgumentException("Positive Integers Only!");
+        } else {
+            return -1;
+        }
     }
 }
