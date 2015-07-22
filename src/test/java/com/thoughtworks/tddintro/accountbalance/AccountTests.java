9ad52929ff9402b4ef81ca9d7366236230f3816e
PatrickDale <patdale216@gmail.com>
2015-07-21 21:56:32 -0700

    Test shouldDecreaseMyBalanceWhenIWithdrawMoney passing.

--- src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
@@ -16,12 +16,17 @@ public class AccountTests {
 
         assertThat(account.getBalance(), is(150));
     }
-//
-//    @Test
-//    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
-//
-//    }
-//
+
+    @Test
+    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
+        Account account = new Account();
+        account.setBalance(100);
+
+        account.withdraw(50);
+
+        assertThat(account.getBalance(), is(50));
+    }
+
 //    @Test
 //    public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
 //
