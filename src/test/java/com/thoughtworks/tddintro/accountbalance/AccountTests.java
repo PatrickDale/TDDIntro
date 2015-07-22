99c00681547a6fa061d5094e52bfdfe3f7653fa4
PatrickDale <patdale216@gmail.com>
2015-07-21 22:09:23 -0700

    Refactor to reduce code repetition. Tests passing.

--- src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
@@ -7,24 +7,29 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class AccountTests {
+    private Account account;
+
+    @Before
+    public void beforeEachTest() {
+        this.account = new Account();
+    }
+
     @Test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-        Account account = new Account();
-        account.setBalance(100);
+        this.account.setBalance(100);
 
-        account.deposit(50);
+        this.account.deposit(50);
 
-        assertThat(account.getBalance(), is(150));
+        assertThat(this.account.getBalance(), is(150));
     }
 
     @Test
     public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
-        Account account = new Account();
-        account.setBalance(100);
+        this.account.setBalance(100);
 
-        account.withdraw(50);
+        this.account.withdraw(50);
 
-        assertThat(account.getBalance(), is(50));
+        assertThat(this.account.getBalance(), is(50));
     }
 
 //    @Test
